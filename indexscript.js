1166547eb477bcd76b9b42c974ad7803749f6478
Thejas T <Thejas@LAP083-Thejas-T.local>
2024-08-02 17:25:11 +0530

    third

--- indexscript.js
+++ indexscript.js
@@ -2,57 +2,22 @@ const apiId = "068e39e4c521f24bcb1f2324e3714319";
 
 let weatherData;
 
-// document.addEventListener("DOMContentLoaded", () => {
 const locationInput = document.querySelector(".search-text");
-const searchButton = document.querySelector(".search-button");
+const searchInput = document.querySelector(".search-input");
 const loadingDiv = document.querySelector(".loading");
 const errorFetching = document.querySelector(".error-fetching");
 const userDenied = document.querySelector(".user-denied");
 const locationButton = document.querySelector(".location-button");
 
-console.log(navigator.geolocation);
-
-// locationButton.addEventListener("click", () => {
-//   if (navigator.geolocation) {
-//     console.log("checking");
-//     navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
-//   } else {
-//     console.log("eroorr");
-//     // locationInfo.textContent = "Geolocation is not supported by this browser.";
-//   }
-// });
-
-// function successCallback(position) {
-//   const latitude = position.coords.latitude;
-//   const longitude = position.coords.longitude;
-
-//   locationInfo.textContent = `Latitude: ${latitude}, Longitude: ${longitude}`;
-// }
-
-// function errorCallback(error) {
-//   switch (error.code) {
-//     case error.PERMISSION_DENIED:
-//       locationInfo.textContent = "User denied the request for Geolocation.";
-//       break;
-//     case error.POSITION_UNAVAILABLE:
-//       locationInfo.textContent = "Location information is unavailable.";
-//       break;
-//     case error.TIMEOUT:
-//       locationInfo.textContent = "The request to get user location timed out.";
-//       break;
-//     case error.UNKNOWN_ERROR:
-//       locationInfo.textContent = "An unknown error occurred.";
-//       break;
-//   }
-// }
-
-searchButton.addEventListener("click", async () => {
+searchInput.addEventListener("submit", async (e) => {
+  e.preventDefault();
   loadingDiv.style.display = "block";
   errorFetching.style.display = "none";
+  userDenied.style.display = "none";
 
   try {
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${locationInput.value}&appid=${apiId}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${locationInput.value}&appid=${apiId}&units=metric`
     );
 
     const data = await response.json();
@@ -63,6 +28,18 @@ searchButton.addEventListener("click", async () => {
     if (!response.ok) {
       throw new Error();
     }
+
+    const queryString = new URLSearchParams({
+      name: data.name,
+      temp: data.main.temp,
+      feels_like: data.main.feels_like,
+      description: data.weather[0].description,
+      humidity: data.main.humidity,
+      icon: data.weather[0].icon,
+    }).toString();
+    console.log(data);
+
+    window.location.href = `weather.html?${queryString}`;
   } catch (error) {
     errorFetching.style.display = "block";
   } finally {
@@ -73,23 +50,43 @@ searchButton.addEventListener("click", async () => {
 
 locationButton.addEventListener("click", async () => {
   loadingDiv.style.display = "block";
+  errorFetching.style.display = "none";
   userDenied.style.display = "none";
+
   try {
-    console.log(latitude);
-    console.log(longitude);
-    const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}.44&${longitude}=-94.04&appid=${apiId}`
-    );
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(async (position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        const response = await fetch(
+          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiId}&units=metric`
+        );
 
-    console.log(response);
+        console.log(response);
 
-    const data = await response.json();
+        const data = await response.json();
 
-    console.log(data.name);
-    weatherData = data;
+        console.log(data.name);
+        weatherData = data;
 
-    if (!response.ok) {
-      throw new Error();
+        if (!response.ok) {
+          userDenied.style.display = "block";
+          throw new Error();
+        }
+        const queryString = new URLSearchParams({
+          name: data.name,
+          temp: data.main.temp,
+          feels_like: data.main.feels_like,
+          description: data.weather[0].description,
+          humidity: data.main.humidity,
+          icon: data.weather[0].icon,
+        }).toString();
+        console.log(data);
+
+        window.location.href = `weather.html?${queryString}`;
+      });
+    } else {
+      userDenied.style.display = "block";
     }
   } catch (error) {
     userDenied.style.display = "block";
@@ -98,5 +95,3 @@ locationButton.addEventListener("click", async () => {
   }
   console.log(weatherData);
 });
-
-// });
--- weatherscript.js
+++ weatherscript.js
@@ -1,7 +1,22 @@
-import { apiId } from "./indexscript.js";
+const backArrow = document.querySelector(".back-arrow");
 
-console.log(apiId);
+backArrow.addEventListener("click", async () => {
+  window.location.href = `index.html`;
+});
 
-const locationName = document.querySelector(".location-name");
+const params = new URLSearchParams(window.location.search);
 
-console.log(locationName);
+const city = params.get("name");
+const temp = params.get("temp");
+const feels_like = params.get("feels_like");
+const description = params.get("description");
+const humidity = params.get("humidity");
+const icon = params.get("icon");
+
+document.querySelector(".location span").textContent = city;
+document.querySelector(".humidity .texts span").textContent = humidity;
+document.querySelector(".feels-like .texts span").textContent = feels_like;
+document.querySelector(".description").textContent = description;
+document.querySelector(".temp span").textContent = temp;
+const newSrc = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+document.querySelector(".weather-img").src = newSrc;
